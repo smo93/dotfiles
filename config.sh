a67319758a8b480802fc167a86c16e2f8a161944
smo93 <georgimpeev@gmail.com>
2014-03-28 18:01:25 +0200

    added script to create sym links to the dotfiles

---
+++ config.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+################################################################################
+# This script creates symlinks from the home dir to some dotfiles              #
+################################################################################
+
+# Variables
+
+dir=~/.dotfiles
+olddir=~/.dotfiles_old
+files="bashrc vimrc vim"
+
+#########################
+
+# create .dotfiles_old to backup any existing dotfiles
+echo "Creating $olddir to back up any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done."
+
+# changing to the .dotfiles dir
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done."
+
+# move any existing dotfile from the ~ dir and create the sym links
+for file in $files; do
+    echo "Moving existing dotfiles from ~ to $olddir."
+    mv ~/.$file $olddir
+    echo "Creating symlink for $file in home dir."
+    ln -s $dir/$file ~/.$file
+done
